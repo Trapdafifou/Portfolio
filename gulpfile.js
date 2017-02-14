801700f611eaf792a667f9adadf34427ed79a166
Trapdafifou <t.marsal@mutum.fr>
2017-02-14 14:25:50 +0100

    [ADD] npm gulp and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-    return gulp.src('./assets/style/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/style/css'));
+gulp.task('scss', function () {
+    return gulp.src('assets/style/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/style/'))
+//    génère un dossier
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./assets/style/*.scss', ['sass']);
-});
+gulp.task('default', ['scss'],function () {
+    console.log('worked')
+})
